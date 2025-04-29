7d7b93fc2fcb5463dee1c4e8b60b2aeea04dded4
axelprenium <axel.studenny@gmail.com>
2025-04-30 00:57:03 +0200

    update(presentation)
    refactored presentation component by removing an image, enhancing content structure, and adding a booking button for improved user interaction. Updated CSS for new layout styles.

--- frontend/src/components/presentation.js
+++ frontend/src/components/presentation.js
@@ -3,10 +3,10 @@ app.component("presentation-component", {
     data() 
     {
         return {
-            images: ["../public/images/mercedes_group.png","../public/images/mercedes_class-S.png","../public/images/mercedes_class-V.png" ],
+            images: ["../public/images/mercedes_group.png","../public/images/mercedes_class-S.png"],
             titles: ["Our Fleet", "Since 2016"],
             contents: [["The latest models of the Mercedes range.", "From a V-class mini van for groups to a S-class long.", " They all have leather interiors, phone chargers, bluetooth and bottles of water."],
-            ["Saint-Tropez Limousines organizes your business and leisure travels throughout the French Riviera. ","We built our reputation over many years on the loyalty of Saint-Tropez’s visitors and business partners.", "Our service will surpass your expectations."],
+            ["Saint-Tropez Limousines organizes your business and leisure travels throughout the French Riviera. ","We built our reputation over many years on the loyalty of Saint-Tropez's visitors and business partners.", "Our service will surpass your expectations."],
             ]
         }
     },
@@ -18,7 +18,13 @@ template:
         <img :src="image"/>
         <div class="inBoxContent">
             <div class="titreTxt">{{titles[index]}}</div>
-            <div v-for="(content, index2) in contents[index]">{{contents[index][index2]}}</div>
+            <div class="button-text">
+            <div class="Txt" v-for="(content, index2) in contents[index]" :key="index2">
+                {{ contents[index][index2] }}
+                <br v-if="index2 < contents[index].length - 1">
+            </div>
+            <button class="bookbutton">Book</button>
+            </div>
         </div>
     </div>
     `
