39b7bffd6808a874bd3c8d48a6ee3a338cf81ea2
axelprenium <axel.studenny@gmail.com>
2025-04-29 15:28:51 +0200

    update(presentation)
    removed a title and adjusted the contents array in the presentation component for clarity

--- frontend/src/components/presentation.js
+++ frontend/src/components/presentation.js
@@ -4,10 +4,10 @@ app.component("presentation-component", {
     {
         return {
             images: ["../public/images/mercedes_group.png","../public/images/mercedes_class-S.png","../public/images/mercedes_class-V.png" ],
-            titles: ["Our Fleet", "Since 2016", "For Testing"],
+            titles: ["Our Fleet", "Since 2016"],
             contents: [["The latest models of the Mercedes range.", "From a V-class mini van for groups to a S-class long.", " They all have leather interiors, phone chargers, bluetooth and bottles of water."],
             ["Saint-Tropez Limousines organizes your business and leisure travels throughout the French Riviera. ","We built our reputation over many years on the loyalty of Saint-Tropez’s visitors and business partners.", "Our service will surpass your expectations."],
-            ["The latest models of the Mercedes range.", "From a V-class mini van for groups to a S-class long.", " They all have leather interiors, phone chargers, bluetooth and bottles of water."]]
+            ]
         }
     },
 
