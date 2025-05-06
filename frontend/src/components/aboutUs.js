216710813c44b1db4748b94448b5777fc39b8ac4
Antoine Louis Marcel Piguet <pn01kbc@eduvaud.ch>
2025-05-06 15:14:06 +0200

    About Us page(page about us, a voir pour le texte a mettre en place)

---
+++ frontend/src/components/aboutUs.js
@@ -0,0 +1,19 @@
+app.component("aboutUs-component", {
+  data() {
+    return {};
+  },
+
+  template:
+    /*html*/
+    `
+      <h1>About Us</h1>
+      <div>
+          <h3>Don’t hesitate to contact us for questions & collaborations.</h3>
+          <p>We offer high-end limousine service all year long. 
+          <br/>We collaborate with touristic partners in the region to plan and personalize your holidays.
+          <br/>Take advantage of our services, whether for your airport transfers, shuttles to the beach, to the village or to organize a day trip at the Côte d'Azur. 
+          <br/>We also take care of any kind of event such as business trips or weddings according to your needs. 
+          <br/>Already present on the Cannes Film Festival, the Monaco Grand Prix or the Longines Athina Onassis Horse Show event we will be your direct point of contact for setting up your projects.</p>
+      </div>
+      `,
+});
---
+++ frontend/src/main4.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+  data() {
+    return {};
+  },
+  template:
+    /*html*/
+    `
+    <div class="templateContainer">
+        <header-component></header-component>
+        <aboutUs-component></aboutUs-component>
+        <footer-component></footer-component>
+    </div>
+`,
+});
